3babbbf01cddbdba395cfa5d195639ec5afc89d1
Raymin <raymin_cabrera23@live.com>
2023-11-17 22:24:14 -0500

    solved 12

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -12,9 +12,41 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here 
+  // Your code here
+  // for (let i = 0; i < arr.length; i++) {
+    // // let ele = arr[i]
+    // if (ele <= arr[1]) {
+    //   arr.slice(1)
+    //   return arr
+
+
+    // }
+    // else {
+    //   return false;
+  //   }
+
+  // }
+  // base case [if the array has less than two items return]
+  if (arr.length < 2){
+    return true;
+
+
+  }
+  // we are checking if the next item in the index is in ascending order or greater
+   if (arr[0] > arr[1]) {
+   return false
+
+
+  }
+  // recursing step and call we are moving towards the base case by shortening our array
+
+return isSorted(arr.slice(1)) // its recursion because the method is called inside itself here 
 }
 
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
